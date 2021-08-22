f5e2a96bdbb7862bb89ca2d07063d917df6271b8
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-22 23:36:37 +0800

    ShiZhengPu 20210544    Static hostname: u20          Icon name: computer-vm            Chassis: vm         Machine ID: 0dd1e64237fd44e5ab187a992c215db2            Boot ID: e4053a2e2555406c89e4d5ee4e8fccfd     Virtualization: oracle   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  23:36:37 up 13:00,  1 user,  load average: 0.66, 0.34, 0.29

---
+++ code/oscpu-local/projects/cpu_diff/vsrc/RAM_1W2R.v
@@ -0,0 +1,31 @@
+module RAM_1W2R(
+    input clk,
+    
+    input [`BUS_WIDTH]inst_addr,
+    input inst_ena,
+    output [31:0]inst,
+
+    // DATA PORT
+    input ram_wr_en,
+    input ram_rd_en,
+    input [`BUS_WIDTH]ram_wmask,
+    input [`BUS_WIDTH]ram_addr,
+    input [`BUS_WIDTH]ram_wr_data,
+    output reg [`BUS_WIDTH]ram_rd_data
+);
+
+    // INST PORT
+
+    wire[`BUS_WIDTH] inst_2 = ram_read_helper(inst_ena,{3'b000,(inst_addr-64'h0000_0000_8000_0000)>>3});
+
+    assign inst = inst_addr[2] ? inst_2[63:32] : inst_2[31:0];
+
+    // DATA PORT 
+    assign ram_rd_data = ram_read_helper(ram_rd_en, {3'b000,(ram_addr-64'h0000_0000_8000_0000)>>3});
+
+    always @(posedge clk) begin
+        ram_write_helper((ram_addr-64'h0000_0000_8000_0000)>>3, ram_wr_data, ram_wmask, ram_wr_en);
+    end
+
+endmodule
+
