49639fc7f028144ee5c1bd1d4b512a4ea5906484
20210544_ShiZhengPu <zhengpushi@126.com>
2021-08-22 22:42:26 +0800

    oscpu-local

---
+++ code/oscpu-local/build.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+
+VERSION="1.15"
+
+help() {
+    echo "Version v"$VERSION
+    echo "Usage:"
+    echo "build.sh [-e project_name] [-b] [-t top_file] [-s] [-a parameters_list] [-f] [-l] [-g] [-w] [-c] [-d] [-m]"
+    echo "Description:"
+    echo "-e: Specify a example project. For example: -e counter. If not specified, the default directory \"cpu\" will be used."
+    echo "-b: Build project using verilator and make tools automatically. It will generate the \"build\"(difftest) or \"build_test\" subfolder under the project directory."
+    echo "-t: Specify a file as verilog top file. If not specified, the default filename \"top.v\" will be used. This option is invalid when connected difftest."
+    echo "-s: Run simulation program. Use the \"build_test\" or \"build\"(difftest) folder as work path."
+    echo "-a: Parameters passed to the simulation program. For example: -a \"1 2 3 ......\". Multiple parameters require double quotes."
+    echo "-f: C++ compiler arguments for makefile. For example: -f \"-DGLOBAL_DEFINE=1 -ggdb3\". Multiple parameters require double quotes. This option is invalid when connected difftest."
+    echo "-l: C++ linker arguments for makefile. For example: -l \"-ldl -lm\". Multiple parameters require double quotes. This option is invalid when connected difftest."
+    echo "-g: Debug the simulation program with GDB. This option is invalid when connected difftest."
+    echo "-w: Open the latest waveform file(.vcd) using gtkwave under work path. Use the \"build_test\" or \"build\"(difftest) folder as work path."
+    echo "-c: Delete \"build\" and \"build_test\" folders under the project directory."
+    echo "-d: Connect to XiangShan difftest framework."
+    echo "-m: Parameters passed to the difftest makefile. For example: -m \"EMU_TRACE=1 EMU_THREADS=4\". Multiple parameters require double quotes."
+    echo "-r: Run all test cases in the \"bin\" directory. This option requires the project to be able to connect to difftest."
+    exit 0
+}
+
+create_soft_link() {
+    mkdir ${1} 1>/dev/null 2>&1
+    find -L ${1} -type l -delete
+    FILES=`eval "find ${2} -name ${3}"`
+    for FILE in ${FILES[@]}
+    do
+        eval "ln -s \"`realpath --relative-to="${1}" "$FILE"`\" \"${1}/${FILE##*/}\" 1>/dev/null 2>&1"
+    done
+}
+
+install_packages() {
+    for ARG in $*                     
+    do
+      [[ ! `dpkg -l | grep $ARG` ]] && sudo apt-get --yes install $ARG
+    done
+}
+
+compile_dramsim3() {
+    if [[ ! -f $OSCPU_PATH/$DRAMSIM3_FOLDER/build/libdramsim3.a ]]; then
+        install_packages cmake
+        [[ ! `dpkg -l | grep cmake` ]] && sudo apt-get --yes install cmake
+        mkdir $OSCPU_PATH/$DRAMSIM3_FOLDER/build
+        cd $OSCPU_PATH/$DRAMSIM3_FOLDER/build
+        cmake -D COSIM=1 ..
+        make
+        if [ $? -ne 0 ]; then
+            echo "Failed to build dramsim3!!!"
+            exit 1
+        fi
+        cd $OSCPU_PATH
+    fi
+}
+
+compile_nemu() {
+    if [[ ! -f $NEMU_HOME/build/riscv64-nemu-interpreter-so ]]; then
+        install_packages libreadline-dev libsdl2-dev bison
+        cd $NEMU_HOME
+        make riscv64-xs-ref_defconfig
+        sed -i 's/CONFIG_MSIZE=0x200000000/CONFIG_MSIZE=0x10000000/' .config
+        make
+        if [ $? -ne 0 ]; then
+            echo "Failed to build nemu!!!"
+            exit 1
+        fi
+        cd $OSCPU_PATH
+    fi
+}
+
+compile_difftest() {
+    cd $DIFFTEST_HOME
+    sed -i 's/#define EMU_RAM_SIZE (8 \* 1024 \* 1024 \* 1024UL)/#define EMU_RAM_SIZE (256 \* 1024 \* 1024UL)/' src/test/csrc/common/ram.h
+    make DESIGN_DIR=$PROJECT_PATH $DIFFTEST_PARAM
+    if [ $? -ne 0 ]; then
+        echo "Failed to build difftest!!!"
+        exit 1
+    fi
+    cd $OSCPU_PATH
+}
+
+build_diff_proj() {
+    # Refresh the modification time of the top file, otherwise some changes to the RTL source code will not take effect in next compilation.
+    touch -m `find $BUILD_PATH -name $DIFFTEST_TOP_FILE` 1>/dev/null 2>&1
+    # create soft link ($BUILD_PATH/*.v -> $PROJECT_PATH/$VSRC_FOLDER/*.v)
+    create_soft_link $BUILD_PATH $PROJECT_PATH/$VSRC_FOLDER \"*.v\"
+    # create soft link ($PROJECT_PATH/difftest -> $LIBRARIES_HOME/difftest)
+    if [[ ! -L $PROJECT_PATH/$DIFFTEST_FOLDER ]]; then
+        eval "ln -s \"`realpath --relative-to="$PROJECT_PATH" "$LIBRARIES_HOME"`/$DIFFTEST_FOLDER\" \"$PROJECT_PATH/$DIFFTEST_FOLDER\" 1>/dev/null 2>&1"
+    fi
+
+    compile_dramsim3
+    compile_nemu
+    compile_difftest
+}
+
+build_proj() {
+    cd $PROJECT_PATH
+
+    # get all .cpp files
+    CSRC_LIST=`find $PROJECT_PATH/$CSRC_FOLDER -name "*.cpp"`
+    for CSRC_FILE in ${CSRC_LIST[@]}
+    do
+        CSRC_FILES="$CSRC_FILES $CSRC_FILE"
+    done
+    # get all vsrc subfolders
+    VSRC_SUB_FOLDER=`find $VSRC_FOLDER -type d`
+    for SUBFOLDER in ${VSRC_SUB_FOLDER[@]}
+    do
+        INCLUDE_VSRC_FOLDERS="$INCLUDE_VSRC_FOLDERS -I$SUBFOLDER"
+    done
+    # get all csrc subfolders
+    CSRC_SUB_FOLDER=`find $PROJECT_PATH/$CSRC_FOLDER -type d`
+    for SUBFOLDER in ${CSRC_SUB_FOLDER[@]}
+    do
+        INCLUDE_CSRC_FOLDERS="$INCLUDE_CSRC_FOLDERS -I$SUBFOLDER"
+    done
+
+    # compile
+    mkdir $BUILD_FOLDER 1>/dev/null 2>&1
+    eval "verilator --x-assign unique --cc --exe --trace --assert -O3 -CFLAGS \"-std=c++11 -Wall $INCLUDE_CSRC_FOLDERS $CFLAGS\" $LDFLAGS -o $PROJECT_PATH/$BUILD_FOLDER/$EMU_FILE \
+        -Mdir $PROJECT_PATH/$BUILD_FOLDER/emu-compile $INCLUDE_VSRC_FOLDERS --build $V_TOP_FILE $CSRC_FILES"
+    if [ $? -ne 0 ]; then
+        echo "Failed to run verilator!!!"
+        exit 1
+    fi
+
+    cd $OSCPU_PATH
+}
+
+# Initialize variables
+OSCPU_PATH=$(dirname $(readlink -f "$0"))
+MYINFO_FILE=$OSCPU_PATH"/myinfo.txt"
+EMU_FILE="emu"
+PROJECT_FOLDER="cpu"
+BUILD_FOLDER="build_test"
+DIFF_BUILD_FOLDER="build"
+VSRC_FOLDER="vsrc"
+CSRC_FOLDER="csrc"
+BIN_FOLDER="bin"
+BUILD="false"
+V_TOP_FILE="top.v"
+SIMULATE="false"
+CHECK_WAVE="false"
+CLEAN="false"
+PARAMETERS=
+CFLAGS=
+LDFLAGS=
+GDB="false"
+LIBRARIES_FOLDER="libraries"
+DIFFTEST="false"
+DIFFTEST_FOLDER="difftest"
+DIFFTEST_PATH=$LIBRARIES_FOLDER/$DIFFTEST_FOLDER
+DIFFTEST_TOP_FILE="SimTop.v"
+NEMU_PATH=$LIBRARIES_FOLDER"/NEMU"
+DIFFTEST_HELPER_PATH="src/test/vsrc/common"
+DIFFTEST_PARAM=
+RUNALL="false"
+DRAMSIM3_FOLDER="libraries/DRAMsim3"
+
+# Check parameters
+while getopts 'he:bt:sa:f:l:gwcdm:r' OPT; do
+    case $OPT in
+        h) help;;
+        e) PROJECT_FOLDER="$OPTARG";;
+        b) BUILD="true";;
+        t) V_TOP_FILE="$OPTARG";;
+        s) SIMULATE="true";;
+        a) PARAMETERS="$OPTARG";;
+        f) CFLAGS="$OPTARG";;
+        l) LDFLAGS="$OPTARG";;
+        g) GDB="true";;
+        w) CHECK_WAVE="true";;
+        c) CLEAN="true";;
+        d) DIFFTEST="true";;
+        m) DIFFTEST_PARAM="$OPTARG";;
+        r) RUNALL="true";;
+        ?) help;;
+    esac
+done
+
+[[ $RUNALL == "true" ]] && DIFFTEST="true"
+[[ $LDFLAGS ]] && LDFLAGS="-LDFLAGS "\"$LDFLAGS\"
+
+PROJECT_PATH=$OSCPU_PATH/projects/$PROJECT_FOLDER
+[[ "$DIFFTEST" == "true" ]] && BUILD_PATH=$PROJECT_PATH/$DIFF_BUILD_FOLDER || BUILD_PATH=$PROJECT_PATH/$BUILD_FOLDER
+[[ "$DIFFTEST" == "true" ]] && V_TOP_FILE=$DIFFTEST_TOP_FILE
+NEMU_HOME=$OSCPU_PATH/$NEMU_PATH
+DIFFTEST_HOME=$OSCPU_PATH/$DIFFTEST_PATH
+DRAMSIM3_HOME=$OSCPU_PATH/$DRAMSIM3_FOLDER
+LIBRARIES_HOME=$OSCPU_PATH/$LIBRARIES_FOLDER
+export NEMU_HOME=$NEMU_HOME
+export NOOP_HOME=$PROJECT_PATH
+export DRAMSIM3_HOME=$DRAMSIM3_HOME
+
+# Get id and name
+ID=`sed '/^ID=/!d;s/.*=//' $MYINFO_FILE`
+NAME=`sed '/^Name=/!d;s/.*=//' $MYINFO_FILE`
+if [[ ${#ID} -le 7 ]] || [[ ${#NAME} -le 1 ]]; then
+    echo "Please fill your information in myinfo.txt!!!"
+    exit 1
+fi
+ID="${ID##*\r}"
+NAME="${NAME##*\r}"
+
+# Clean
+if [[ "$CLEAN" == "true" ]]; then
+    rm -rf $PROJECT_PATH/$BUILD_FOLDER $PROJECT_PATH/$DIFF_BUILD_FOLDER
+    unlink $PROJECT_PATH/$DIFFTEST_FOLDER 1>/dev/null 2>&1
+    exit 0
+fi
+
+# Build project
+if [[ "$BUILD" == "true" ]]; then
+    [[ "$DIFFTEST" == "true" ]] && build_diff_proj || build_proj
+
+    #git commit
+    git add . -A --ignore-errors
+    (echo $NAME && echo $ID && hostnamectl && uptime) | git commit -F - -q --author='tracer-oscpu2021 <tracer@oscpu.org>' --no-verify --allow-empty 1>/dev/null 2>&1
+    sync
+fi
+
+# Simulate
+if [[ "$SIMULATE" == "true" ]]; then
+    cd $BUILD_PATH
+
+    # create soft link ($BUILD_PATH/*.bin -> $OSCPU_PATH/$BIN_FOLDER/*.bin). Why? Because of laziness!
+    create_soft_link $BUILD_PATH $OSCPU_PATH/$BIN_FOLDER \"*.bin\"
+
+    # run simulation program
+    echo "Simulating..."
+    [[ "$GDB" == "true" ]] && gdb -s $EMU_FILE --args ./$EMU_FILE $PARAMETERS || ./$EMU_FILE $PARAMETERS
+
+    if [ $? -ne 0 ]; then
+        echo "Failed to simulate!!!"
+        FAILED="true"
+    fi
+
+    cd $OSCPU_PATH
+fi
+
+# Check waveform
+if [[ "$CHECK_WAVE" == "true" ]]; then
+    cd $BUILD_PATH
+    gtkwave `ls -t | grep .vcd | head -n 1`
+    if [ $? -ne 0 ]; then
+        echo "Failed to run gtkwave!!!"
+        exit 1
+    fi
+    cd $OSCPU_PATH
+fi
+
+[[ "$FAILED" == "true" ]] && exit 1
+
+# Run all
+if [[ $RUNALL == "true" ]]; then
+    cd $BUILD_PATH
+
+    create_soft_link $BUILD_PATH $OSCPU_PATH/$BIN_FOLDER \"*.bin\"
+
+    mkdir log 1>/dev/null 2>&1
+    BIN_FILES=`ls *.bin`
+
+    for BIN_FILE in $BIN_FILES; do
+        FILE_NAME=${BIN_FILE%.*}
+        printf "[%30s] " $FILE_NAME
+        LOG_FILE=log/$FILE_NAME-log.txt
+        ./$EMU_FILE -i $BIN_FILE &> $LOG_FILE
+        if (grep 'HIT GOOD TRAP' $LOG_FILE > /dev/null) then
+            echo -e "\033[1;32mPASS!\033[0m"
+            rm $LOG_FILE
+        else
+            echo -e "\033[1;31mFAIL!\033[0m see $BUILD_PATH/$LOG_FILE for more information"
+        fi
+    done
+
+    cd $OSCPU_PATH
+fi
---
+++ code/oscpu-local/projects/cpu_axi_diff/vsrc/SimTop.v
@@ -0,0 +1,222 @@
+
+`include "defines.v"
+`define AXI_TOP_INTERFACE(name) io_memAXI_0_``name
+
+module SimTop(
+    input                               clock,
+    input                               reset,
+
+    input  [63:0]                       io_logCtrl_log_begin,
+    input  [63:0]                       io_logCtrl_log_end,
+    input  [63:0]                       io_logCtrl_log_level,
+    input                               io_perfInfo_clean,
+    input                               io_perfInfo_dump,
+
+    output                              io_uart_out_valid,
+    output [7:0]                        io_uart_out_ch,
+    output                              io_uart_in_valid,
+    input  [7:0]                        io_uart_in_ch,
+
+    input                               `AXI_TOP_INTERFACE(aw_ready),
+    output                              `AXI_TOP_INTERFACE(aw_valid),
+    output [`AXI_ADDR_WIDTH-1:0]        `AXI_TOP_INTERFACE(aw_bits_addr),
+    output [2:0]                        `AXI_TOP_INTERFACE(aw_bits_prot),
+    output [`AXI_ID_WIDTH-1:0]          `AXI_TOP_INTERFACE(aw_bits_id),
+    output [`AXI_USER_WIDTH-1:0]        `AXI_TOP_INTERFACE(aw_bits_user),
+    output [7:0]                        `AXI_TOP_INTERFACE(aw_bits_len),
+    output [2:0]                        `AXI_TOP_INTERFACE(aw_bits_size),
+    output [1:0]                        `AXI_TOP_INTERFACE(aw_bits_burst),
+    output                              `AXI_TOP_INTERFACE(aw_bits_lock),
+    output [3:0]                        `AXI_TOP_INTERFACE(aw_bits_cache),
+    output [3:0]                        `AXI_TOP_INTERFACE(aw_bits_qos),
+    
+    input                               `AXI_TOP_INTERFACE(w_ready),
+    output                              `AXI_TOP_INTERFACE(w_valid),
+    output [`AXI_DATA_WIDTH-1:0]        `AXI_TOP_INTERFACE(w_bits_data)         [3:0],
+    output [`AXI_DATA_WIDTH/8-1:0]      `AXI_TOP_INTERFACE(w_bits_strb),
+    output                              `AXI_TOP_INTERFACE(w_bits_last),
+    
+    output                              `AXI_TOP_INTERFACE(b_ready),
+    input                               `AXI_TOP_INTERFACE(b_valid),
+    input  [1:0]                        `AXI_TOP_INTERFACE(b_bits_resp),
+    input  [`AXI_ID_WIDTH-1:0]          `AXI_TOP_INTERFACE(b_bits_id),
+    input  [`AXI_USER_WIDTH-1:0]        `AXI_TOP_INTERFACE(b_bits_user),
+
+    input                               `AXI_TOP_INTERFACE(ar_ready),
+    output                              `AXI_TOP_INTERFACE(ar_valid),
+    output [`AXI_ADDR_WIDTH-1:0]        `AXI_TOP_INTERFACE(ar_bits_addr),
+    output [2:0]                        `AXI_TOP_INTERFACE(ar_bits_prot),
+    output [`AXI_ID_WIDTH-1:0]          `AXI_TOP_INTERFACE(ar_bits_id),
+    output [`AXI_USER_WIDTH-1:0]        `AXI_TOP_INTERFACE(ar_bits_user),
+    output [7:0]                        `AXI_TOP_INTERFACE(ar_bits_len),
+    output [2:0]                        `AXI_TOP_INTERFACE(ar_bits_size),
+    output [1:0]                        `AXI_TOP_INTERFACE(ar_bits_burst),
+    output                              `AXI_TOP_INTERFACE(ar_bits_lock),
+    output [3:0]                        `AXI_TOP_INTERFACE(ar_bits_cache),
+    output [3:0]                        `AXI_TOP_INTERFACE(ar_bits_qos),
+    
+    output                              `AXI_TOP_INTERFACE(r_ready),
+    input                               `AXI_TOP_INTERFACE(r_valid),
+    input  [1:0]                        `AXI_TOP_INTERFACE(r_bits_resp),
+    input  [`AXI_DATA_WIDTH-1:0]        `AXI_TOP_INTERFACE(r_bits_data)         [3:0],
+    input                               `AXI_TOP_INTERFACE(r_bits_last),
+    input  [`AXI_ID_WIDTH-1:0]          `AXI_TOP_INTERFACE(r_bits_id),
+    input  [`AXI_USER_WIDTH-1:0]        `AXI_TOP_INTERFACE(r_bits_user)
+);
+
+    wire aw_ready;
+    wire aw_valid;
+    wire [`AXI_ADDR_WIDTH-1:0] aw_addr;
+    wire [2:0] aw_prot;
+    wire [`AXI_ID_WIDTH-1:0] aw_id;
+    wire [`AXI_USER_WIDTH-1:0] aw_user;
+    wire [7:0] aw_len;
+    wire [2:0] aw_size;
+    wire [1:0] aw_burst;
+    wire aw_lock;
+    wire [3:0] aw_cache;
+    wire [3:0] aw_qos;
+    wire [3:0] aw_region;
+
+    wire w_ready;
+    wire w_valid;
+    wire [`AXI_DATA_WIDTH-1:0] w_data;
+    wire [`AXI_DATA_WIDTH/8-1:0] w_strb;
+    wire w_last;
+    wire [`AXI_USER_WIDTH-1:0] w_user;
+    
+    wire b_ready;
+    wire b_valid;
+    wire [1:0] b_resp;
+    wire [`AXI_ID_WIDTH-1:0] b_id;
+    wire [`AXI_USER_WIDTH-1:0] b_user;
+
+    wire ar_ready;
+    wire ar_valid;
+    wire [`AXI_ADDR_WIDTH-1:0] ar_addr;
+    wire [2:0] ar_prot;
+    wire [`AXI_ID_WIDTH-1:0] ar_id;
+    wire [`AXI_USER_WIDTH-1:0] ar_user;
+    wire [7:0] ar_len;
+    wire [2:0] ar_size;
+    wire [1:0] ar_burst;
+    wire ar_lock;
+    wire [3:0] ar_cache;
+    wire [3:0] ar_qos;
+    wire [3:0] ar_region;
+    
+    wire r_ready;
+    wire r_valid;
+    wire [1:0] r_resp;
+    wire [`AXI_DATA_WIDTH-1:0] r_data;
+    wire r_last;
+    wire [`AXI_ID_WIDTH-1:0] r_id;
+    wire [`AXI_USER_WIDTH-1:0] r_user;
+
+    assign ar_ready                                 = `AXI_TOP_INTERFACE(ar_ready);
+    assign `AXI_TOP_INTERFACE(ar_valid)             = ar_valid;
+    assign `AXI_TOP_INTERFACE(ar_bits_addr)         = ar_addr;
+    assign `AXI_TOP_INTERFACE(ar_bits_prot)         = ar_prot;
+    assign `AXI_TOP_INTERFACE(ar_bits_id)           = ar_id;
+    assign `AXI_TOP_INTERFACE(ar_bits_user)         = ar_user;
+    assign `AXI_TOP_INTERFACE(ar_bits_len)          = ar_len;
+    assign `AXI_TOP_INTERFACE(ar_bits_size)         = ar_size;
+    assign `AXI_TOP_INTERFACE(ar_bits_burst)        = ar_burst;
+    assign `AXI_TOP_INTERFACE(ar_bits_lock)         = ar_lock;
+    assign `AXI_TOP_INTERFACE(ar_bits_cache)        = ar_cache;
+    assign `AXI_TOP_INTERFACE(ar_bits_qos)          = ar_qos;
+    
+    assign `AXI_TOP_INTERFACE(r_ready)              = r_ready;
+    assign r_valid                                  = `AXI_TOP_INTERFACE(r_valid);
+    assign r_resp                                   = `AXI_TOP_INTERFACE(r_bits_resp);
+    assign r_data                                   = `AXI_TOP_INTERFACE(r_bits_data)[0];
+    assign r_last                                   = `AXI_TOP_INTERFACE(r_bits_last);
+    assign r_id                                     = `AXI_TOP_INTERFACE(r_bits_id);
+    assign r_user                                   = `AXI_TOP_INTERFACE(r_bits_user);
+
+    axi_rw u_axi_rw (
+        .clock                          (clock),
+        .reset                          (reset),
+
+        .rw_valid_i                     (if_valid),
+        .rw_ready_o                     (if_ready),
+        .rw_req_i                       (req),
+        .data_read_o                    (if_data_read),
+        .data_write_i                   (data_write),
+        .rw_addr_i                      (if_addr),
+        .rw_size_i                      (if_size),
+        .rw_resp_o                      (if_resp),
+
+        .axi_aw_ready_i                 (aw_ready),
+        .axi_aw_valid_o                 (aw_valid),
+        .axi_aw_addr_o                  (aw_addr),
+        .axi_aw_prot_o                  (aw_prot),
+        .axi_aw_id_o                    (aw_id),
+        .axi_aw_user_o                  (aw_user),
+        .axi_aw_len_o                   (aw_len),
+        .axi_aw_size_o                  (aw_size),
+        .axi_aw_burst_o                 (aw_burst),
+        .axi_aw_lock_o                  (aw_lock),
+        .axi_aw_cache_o                 (aw_cache),
+        .axi_aw_qos_o                   (aw_qos),
+        .axi_aw_region_o                (aw_region),
+
+        .axi_w_ready_i                  (w_ready),
+        .axi_w_valid_o                  (w_valid),
+        .axi_w_data_o                   (w_data),
+        .axi_w_strb_o                   (w_strb),
+        .axi_w_last_o                   (w_last),
+        .axi_w_user_o                   (w_user),
+        
+        .axi_b_ready_o                  (b_ready),
+        .axi_b_valid_i                  (b_valid),
+        .axi_b_resp_i                   (b_resp),
+        .axi_b_id_i                     (b_id),
+        .axi_b_user_i                   (b_user),
+
+        .axi_ar_ready_i                 (ar_ready),
+        .axi_ar_valid_o                 (ar_valid),
+        .axi_ar_addr_o                  (ar_addr),
+        .axi_ar_prot_o                  (ar_prot),
+        .axi_ar_id_o                    (ar_id),
+        .axi_ar_user_o                  (ar_user),
+        .axi_ar_len_o                   (ar_len),
+        .axi_ar_size_o                  (ar_size),
+        .axi_ar_burst_o                 (ar_burst),
+        .axi_ar_lock_o                  (ar_lock),
+        .axi_ar_cache_o                 (ar_cache),
+        .axi_ar_qos_o                   (ar_qos),
+        .axi_ar_region_o                (ar_region),
+        
+        .axi_r_ready_o                  (r_ready),
+        .axi_r_valid_i                  (r_valid),
+        .axi_r_resp_i                   (r_resp),
+        .axi_r_data_i                   (r_data),
+        .axi_r_last_i                   (r_last),
+        .axi_r_id_i                     (r_id),
+        .axi_r_user_i                   (r_user)
+    );
+
+    wire if_valid;
+    wire if_ready;
+    wire req = `REQ_READ;
+    wire [63:0] if_data_read;
+    wire [63:0] data_write;
+    wire [63:0] if_addr;
+    wire [1:0] if_size;
+    wire [1:0] if_resp;
+
+    cpu u_cpu(
+        .clock                          (clock),
+        .reset                          (reset),
+
+        .if_valid                       (if_valid),
+        .if_ready                       (if_ready),
+        .if_data_read                   (if_data_read),
+        .if_addr                        (if_addr),
+        .if_size                        (if_size),
+        .if_resp                        (if_resp)
+    );
+
+
+endmodule
