2f14434f97cdda0f3b4b44c0ed86bff71aa2da25
= <=>
2021-08-24 16:39:55 +0800

    git remove oscpu-old

--- code/oscpu-v2/libraries/NEMU/src/isa/riscv32/include/isa-exec.h
+++
@@ -1,8 +0,0 @@
-#include "../instr/pseudo.h"
-#include "../instr/compress.h"
-#include "../instr/compute.h"
-#include "../instr/control.h"
-#include "../instr/ldst.h"
-#include "../instr/muldiv.h"
-#include "../instr/system.h"
-#include "../instr/special.h"
