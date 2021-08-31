03c0e11588603280b63862b9ad20bc3379d5e532
= <=>
2021-08-31 16:16:31 +0800

    feat: am-kernels/benchmarks/dhrystone 50000次40秒

--- code/oscpu/libraries/am-kernels/benchmarks/dhrystone/dry.c
+++ code/oscpu/libraries/am-kernels/benchmarks/dhrystone/dry.c
@@ -358,7 +358,7 @@ static uint32_t uptime_ms() { return io_read(AM_TIMER_UPTIME).us / 1000; }
 #define Start_Timer() Begin_Time = uptime_ms()
 #define Stop_Timer()  End_Time   = uptime_ms()
 
-#define NUMBER_OF_RUNS		500000 /* Default number of runs */
+#define NUMBER_OF_RUNS		50000 //500000 /* Default number of runs */
 #define PASS2
 
 #ifdef  NOSTRUCTASSIGN
