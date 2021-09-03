7fe1c4efed775de20b5634ceefcc8288713f4f9c
= <=>
2021-09-03 10:59:34 +0800

    feat: 开始接入axi；寄存器别名便于调试

--- code/oscpu/projects/cpu_axi_diff/vsrc/regfile.v
+++ code/oscpu/projects/cpu_axi_diff/vsrc/regfile.v
@@ -22,6 +22,40 @@ module regfile(
 
     // 32 registers
 	reg [`REG_BUS] 	regs[0 : 31];
+
+  // register alias name
+  wire [`REG_BUS] x01_zero  = regs[01];
+  wire [`REG_BUS] x02_ra    = regs[02];
+  wire [`REG_BUS] x03_sp    = regs[03];
+  wire [`REG_BUS] x04_gp    = regs[04];
+  wire [`REG_BUS] x05_tp    = regs[05];
+  wire [`REG_BUS] x06_t0    = regs[06];
+  wire [`REG_BUS] x07_t1    = regs[07];
+  wire [`REG_BUS] x08_s0    = regs[08];
+  wire [`REG_BUS] x09_s1    = regs[09];
+  wire [`REG_BUS] x10_a0    = regs[10];
+  wire [`REG_BUS] x11_a1    = regs[11];
+  wire [`REG_BUS] x12_a2    = regs[12];
+  wire [`REG_BUS] x13_a3    = regs[13];
+  wire [`REG_BUS] x14_a4    = regs[14];
+  wire [`REG_BUS] x15_a5    = regs[15];
+  wire [`REG_BUS] x16_a6    = regs[16];
+  wire [`REG_BUS] x17_a7    = regs[17];
+  wire [`REG_BUS] x18_s2    = regs[18];
+  wire [`REG_BUS] x19_s3    = regs[19];
+  wire [`REG_BUS] x20_s4    = regs[20];
+  wire [`REG_BUS] x21_s5    = regs[21];
+  wire [`REG_BUS] x22_s6    = regs[22];
+  wire [`REG_BUS] x23_s7    = regs[23];
+  wire [`REG_BUS] x24_s8    = regs[24];
+  wire [`REG_BUS] x25_s9    = regs[25];
+  wire [`REG_BUS] x26_s10   = regs[26];
+  wire [`REG_BUS] x27_s11   = regs[27];
+  wire [`REG_BUS] x28_t3    = regs[28];
+  wire [`REG_BUS] x29_t4    = regs[29];
+  wire [`REG_BUS] x30_t5    = regs[30];
+  wire [`REG_BUS] x31_56    = regs[31];
+
 	
 	always @(posedge clk) 
 	begin
