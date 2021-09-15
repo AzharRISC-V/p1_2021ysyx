e2d1b9953f363f4d477913b7254136412f9e02cf
= <=>
2021-09-15 10:03:17 +0800

    feat: 取指阶段冲刷流水线更新为“忽略下一条取指”

--- code/oscpu/projects/cpu_axi_diff/vsrc/pipeline/ifU.v
+++ code/oscpu/projects/cpu_axi_diff/vsrc/pipeline/ifU.v
@@ -34,8 +34,22 @@ assign o_bus_req = 1'b1;
 
 wire handshake_done = o_bus_req & i_bus_ack;
 
-// 是否冲刷流水线
-wire flush_en = i_pc_jmp & (i_pc_jmpaddr != o_pc_pred);
+// 忽略下一条取指
+reg           ignore_next_inst;
+reg [`BUS_64] saved_pc_jmpaddr;
+
+always @(posedge clk) begin
+  if (rst) begin
+    ignore_next_inst <= 0;
+    saved_pc_jmpaddr <= 0;
+  end
+  else begin
+    if (i_pc_jmp & (i_pc_jmpaddr != o_pc_pred)) begin
+      ignore_next_inst <= 1;
+      saved_pc_jmpaddr <= i_pc_jmpaddr;
+    end
+  end
+end
 
 // fetch an instruction
 always @( posedge clk ) begin
@@ -43,29 +57,26 @@ always @( posedge clk ) begin
     o_bus_addr              <= `PC_START;
     o_pc                    <= 0;
     o_pc_pred               <= 0;
-    o_nocmt                 <= 0;
     o_fetched               <= 0;
   end
   else begin
-    if (flush_en) begin
-      o_bus_addr              <= i_pc_jmpaddr;
-      o_pc                    <= i_pc_jmpaddr;
-      o_pc_pred               <= i_pc_jmpaddr + 4;
-      o_inst                  <= `INST_NOP;
-      o_nocmt                 <= 1;
-      o_fetched               <= 1;
-    end
-    else if (handshake_done) begin
-      o_bus_addr              <= o_bus_addr + 4;
-      o_pc                    <= o_bus_addr;
-      o_pc_pred               <= o_bus_addr + 4;
-      o_inst                  <= i_bus_rdata[31:0];
-      o_nocmt                 <= 0;
-      o_fetched               <= 1;
+    if (handshake_done) begin
+      if (ignore_next_inst) begin
+        ignore_next_inst        <= 0;
+        o_bus_addr              <= saved_pc_jmpaddr;
+        o_pc                    <= saved_pc_jmpaddr;
+        o_pc_pred               <= saved_pc_jmpaddr + 4;
+      end
+      else begin
+        o_bus_addr              <= o_bus_addr + 4;
+        o_pc                    <= o_bus_addr;
+        o_pc_pred               <= o_bus_addr + 4;
+        o_inst                  <= i_bus_rdata[31:0];
+        o_fetched               <= 1;
+      end
     end
     else begin
       o_inst                  <= 0;
-      o_nocmt                 <= 0;
       o_fetched               <= 0;
     end
   end
